Wrench-1/Vanderlin

commit 96fc43c10e67c477142fa6a314c132b02186ddfa

Child Sleeve Fix / Dwarf Belt Offset Fix (#4404)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

## About The Pull Request

<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->
Fixes two sprite rendering issues. The less noteworthy one is that male
dwarves had a sprite offset in their belts despite having their own
belts. This would put their belts at their feet.
The main fix is switching children to render their own sleeves instead
of the adult variant. In-game, this would result in hunch-back children
with extremely high shoulders. You can see the difference below.
<img width="348" height="349" alt="image"
src="https://github.com/user-attachments/assets/57072d3f-208e-4ad2-b2fc-4a315e8ac7b7"
/>
<img width="213" height="216" alt="image"
src="https://github.com/user-attachments/assets/5feeafc4-97fc-48a5-a830-156bf7a383f4"
/>



## Why It's Good For The Game

<!-- Argue for the merits of your changes and how they benefit the game,
especially if they are controversial and/or far reaching. If you can't
actually explain WHY what you are doing will improve the game, then it
probably isn't good for the game in the first place. -->
Bug fixes

## Changelog

<!-- If your PR modifies aspects of the game that can be concretely
observed by players or admins you should add a changelog. If your change
does NOT meet this description, remove this section. Please note that
maintainers freely reserve the right to remove and add tags should they
deem it appropriate. You can attempt to finagle the system all you want,
but it's best to shoot for clear communication right off the bat. -->

<!-- !! Do not add whitespace in-between the entries, do not change the
tags and do not leave the tags without any entries. -->
:cl:
fix: Fixed children rendering adult sleeves, resulting in hunchbacks.
Fixed male dwarves having belts at their feet.
/:cl:

code/modules/mob/living/carbon/human/update_icons.dm
@@ -1458,13 +1458,15 @@ generate/load female uniform sprites matching all previously decided variables
 		if(sleeveindex == 4 || sleeveindex == 3)
 			sleeveindex -= 2
 
-	var/racecustom
+	var/index = I.icon_state
+	var/mob/living/carbon/human/HM = src
+	if(istype(HM) && HM.age == AGE_CHILD)
+		index += "_child"
+	else if(gender == FEMALE ^ dna.species.swap_female_clothes)
+		index += "_f"
 	if(dna.species.custom_clothes)
-		if(dna.species.custom_id)
-			racecustom = dna.species.custom_id
-		else
-			racecustom = dna.species.id
-	var/index = "[I.icon_state][((gender == FEMALE && !dna.species.swap_female_clothes) || dna.species.swap_male_clothes) ? "_f" : ""][racecustom ? "_[racecustom]" : ""]"
+		index += "_[dna.species.custom_id ? dna.species.custom_id : dna.species.id]"
+
 	var/static/list/bloody_r = list()
 	var/static/list/bloody_l = list()
 	if(I.nodismemsleeves && sleeveindex) //armor pauldrons that show up above arms but don't get dismembered

code/modules/mob/living/carbon/human/species_types/dwarf/dwarfm.dm
@@ -59,7 +59,7 @@
 		OFFSET_FACEMASK = list(0,-4),\
 		OFFSET_HEAD = list(0,-4),\
 		OFFSET_FACE = list(0,-4),\
-		OFFSET_BELT = list(0,-5),\
+		OFFSET_BELT = list(0,0),\
 		OFFSET_BACK = list(0,-4),\
 		OFFSET_NECK = list(0,-4),\
 		OFFSET_MOUTH = list(0,-4),\

commit bcc855ace011d049e8fc2b1af7e9c7aa3c4b3b22

Child Cloak Fix (#4489)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

## About The Pull Request

<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->
Fixes an issue caused accidentally in #4404

## Why It's Good For The Game

<!-- Argue for the merits of your changes and how they benefit the game,
especially if they are controversial and/or far reaching. If you can't
actually explain WHY what you are doing will improve the game, then it
probably isn't good for the game in the first place. -->

## Changelog

<!-- If your PR modifies aspects of the game that can be concretely
observed by players or admins you should add a changelog. If your change
does NOT meet this description, remove this section. Please note that
maintainers freely reserve the right to remove and add tags should they
deem it appropriate. You can attempt to finagle the system all you want,
but it's best to shoot for clear communication right off the bat. -->

<!-- !! Do not add whitespace in-between the entries, do not change the
tags and do not leave the tags without any entries. -->
:cl:
fix: Child Cloaks
/:cl:

<!-- Both :cl:'s are required for the changelog to work! You can put
your name to the right of the first :cl: if you want to overwrite your
GitHub username as author ingame. -->
<!-- You can use multiple of the same prefix (they're only used for the
icon ingame) and delete the unneeded ones. Despite some of the tags,
changelogs should generally represent how a player might be affected by
the changes rather than a summary of the PR's contents. -->

## Pre-Merge Checklist
<!-- Don't bother filling these in while creating your Pull Request,
just click the checkboxes after the Pull Request is opened and you are
redirected to the page. -->
- [x] You tested this on a local server.
- [x] This code did not runtime during testing.

code/modules/mob/living/carbon/human/update_icons.dm
@@ -47,6 +47,8 @@ There are several things that need to be remembered:
 
 
 */
+GLOBAL_LIST_INIT(no_child_icons, list(/obj/item/clothing/head, /obj/item/clothing/face, /obj/item/clothing/cloak, /obj/item/clothing/gloves, /obj/item/clothing/neck))
+GLOBAL_PROTECT(no_child_icons)
 
 /mob/living/carbon/proc/get_limbloss_index(limbr, limbl)
 	var/jazz = 1
@@ -1350,9 +1352,8 @@ generate/load female uniform sprites matching all previously decided variables
 		if(sleevejazz)
 			sleevejazz += "_[customi]"
 	var/t_icon = mob_overlay_icon
-	if(age == AGE_CHILD)
-		if(!istype(src, /obj/item/clothing/head) && !istype(src, /obj/item/clothing/face) && !istype(src, /obj/item/clothing/cloak) && !istype(src, /obj/item/clothing/gloves) && !istype(src, /obj/item/clothing/neck))
-			t_state += "_child"
+	if(age == AGE_CHILD && !is_type_in_list(src, GLOB.no_child_icons))
+		t_state += "_child"
 	if(!t_icon)
 		t_icon = default_icon_file
 
@@ -1460,7 +1461,7 @@ generate/load female uniform sprites matching all previously decided variables
 
 	var/index = I.icon_state
 	var/mob/living/carbon/human/HM = src
-	if(istype(HM) && HM.age == AGE_CHILD)
+	if(istype(HM) && HM.age == AGE_CHILD && !is_type_in_list(I, GLOB.no_child_icons))
 		index += "_child"
 	else if(gender == FEMALE ^ dna.species.swap_female_clothes)
 		index += "_f"